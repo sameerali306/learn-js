881fc29a69ac1632ebac06486c645d26d6e0ea7d
sameerali306 <sameeraly240@gmail.com>
2025-05-10 21:56:20 +0500

    first commit

---
+++ basic_js/01_basic.js
@@ -0,0 +1,11 @@
+const accountId=12233546
+let accountEmail="sameer@email.com"
+var accountcity="gilgit"
+accountpassword=2300101
+
+// accountId=453536  //accountId cannot be change bcz it is constant
+accountEmail="samir@gmail.com"
+accountcity="jandrote"
+accountpassword=53444362
+
+console.table([accountEmail,accountId,accountcity,accountpassword])
