ba211e50feab88032c594ea7affaae87446cd556
sameerali306 <sameeraly240@gmail.com>
2025-05-11 12:20:46 +0500

    second commit

---
+++ basic_js/02_basic.js
@@ -0,0 +1,13 @@
+// data type and their use
+//mathmatical calculation
+let a=2+5
+
+// counter and loops
+for (let i = 0; i < 10; i++) {
+   console.log(i);
+   
+    
+
+}
+
+// time and delays
---
+++ basic_js/03_basic.js
@@ -0,0 +1,32 @@
+let IsloggedIn=true
+
+if (IsloggedIn) {
+    console.log("you are loggedd in");
+    
+    
+} else {
+    console.log("you are not logged in");
+    
+    
+}
+
+// conersion of data type
+let score=false
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let loggedIn=1
+let result=Boolean(loggedIn)
+console.log(result);
+
+let number=33
+let ans=String(number)
+console.log(typeof ans);
+
+
+
+
+
