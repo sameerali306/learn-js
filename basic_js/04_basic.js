bfddb54ce335454096d03607b811e8da7364a26d
sameerali306 <sameeraly240@gmail.com>
2025-05-12 15:57:02 +0500

    third commit

---
+++ basic_js/04_basic.js
@@ -0,0 +1,38 @@
+let str1="hello"
+let str2="sameer ali"
+let str3=str1 + str2
+// console.log(str3);/
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2"+4);
+
+let gameCounter=1
+gameCounter++
+// console.log(gameCounter);
+
+let x=2
+let y=x++
+// console.log(`x:${x} and y:${y}`);
+
+let z=2
+let m=++z
+// console.log(`z:${z} and m:${m}`);
+
+// ******************compresion operTORE********************
+
+console.log(null==0);
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);
+
+
+
+
+
+
+
+
+
+
