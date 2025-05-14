807a54cd97c2e81dae759ffaa0c89482c4da7381
sameerali306 <sameeraly240@gmail.com>
2025-05-14 17:52:57 +0500

    origin

---
+++ basic_js/08_basic.js
@@ -0,0 +1,37 @@
+let score=400  //in this the javascript automatically find the type of this is number
+// console.log(score);
+let num=new Number(400)  //in this we define the type is number 
+// console.log(num);
+// console.log(num.toFixed(2));
+
+const nmbr=123.22
+// console.log(nmbr.toPrecision(3)); //it the precision of the number by round off decimal point
+
+const hunders=10000000
+// console.log(hunders.toLocaleString("en-IN"));
+// console.log(hunders.toLocaleString())
+// console.log(hunders.toLocaleString("en-PK"));
+
+//***************************************MATHS */
+console.log(Math)
+console.log(Math.abs(-4)); //this absolute value change the alue from negative to postive and postive to negative
+console.log(Math.round(4.67));  //this can round of the number to one decimal place
+console.log(Math.ceil(4.2));   //the ceil method of the maths give large number that is in the decimal point
+console.log(Math.floor(4.2)); //the floor method of maths give small  number in the number tht is give in the decimal point
+console.log(Math.max(2,3,4,5,6)); //this find the large number from alll of this number
+console.log(Math.min(2,3,4,5,6)); //this give the small number from all of the above number
+console.log(Math.random()); //this mthod will generate the andom number batween 1 and 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
