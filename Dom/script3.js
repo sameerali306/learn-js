59556452973f7f926a5c39695f0252a19ee408d7
sameerali306 <sameeraly240@gmail.com>
2025-05-27 22:51:41 +0500

    origin

--- Dom/script3.js
+++ Dom/script3.js
@@ -1,15 +1,50 @@
-const parent=document.querySelector(".parent")
-console.log(parent);
-console.log(parent.children[2]);
-for (let i = 0; i < parent.children.length; i++) {
-    const element = parent.children[i].innerHTML;
-    console.log(element);
+// const parent=document.querySelector(".parent")
+// console.log(parent);
+// console.log(parent.children[2]);
+// for (let i = 0; i < parent.children.length; i++) {
+//     const element = parent.children[i].innerHTML;
+//     console.log(element);
     
     
+// }
+// parent.children[1].style.color="red"
+// console.log(parent.secondElementChild);
+// const day=document.querySelector(".child")
+// console.log(day);
+// console.log(day.parentElement);
+// console.log(day.nextElementSibling);
+
+function global() {
+    console.log("clicked");
+    
+  const div = document.createElement("div");
+  div.className = "items";
+  div.id = "div-items";
+
+  div.innerHTML = `
+    <h1 class="header">sameer ali</h1>
+    <p id="paragraph">my name is sameer ali and I am from jandrote</p>
+    <button>click me</button>
+  `;
+
+  // Append first, then select inside the new div
+  document.body.appendChild(div);
+
+  // Select inside this new div only
+  const header = div.querySelector(".header");
+  header.style.color = "yellow";
+
+  const paragraph = div.querySelector("#paragraph");
+  paragraph.style.color = "red";
+
+  div.style.background = "green";
+  div.style.width = "200px";   // Add units!
+  div.style.padding = "12px";
 }
-parent.children[1].style.color="red"
-console.log(parent.secondElementChild);
-const day=document.querySelector(".child")
-console.log(day);
-console.log(day.parentElement);
-console.log(day.nextElementSibling);
+
+
+const div = document.createElement("div");
+div.innerHTML = `<button onclick="global()">click me</button>`;
+document.body.appendChild(div);
+
+
