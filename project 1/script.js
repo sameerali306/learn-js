1cd6adef893dcbd3161e86e23bf3212621862abc
sameerali306 <sameeraly240@gmail.com>
2025-05-30 18:58:52 +0500

    origin

--- project 1/script.js
+++ project 1/script.js
@@ -5,6 +5,8 @@ form.addEventListener("submit", function(e) {
     const height = parseInt(document.querySelector("#height").value);
     const weight = parseInt(document.querySelector("#weight").value);
     const results = document.querySelector("#results");
+    const bmiresults = document.querySelector("#bmiresults");
+
 
     if (height === "" || height < 0 || isNaN(height)) {
         results.innerHTML = `Please give a valid height: ${height}`;
@@ -18,5 +20,17 @@ form.addEventListener("submit", function(e) {
 
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     results.innerHTML = `Your BMI is ${bmi}`;
+
+    if (bmi < 18.5) {
+    bmiresults.innerHTML = "You are underweight.";
+    bmiresults.style.color = "red";
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+    bmiresults.innerHTML = "You are at a normal weight.";
+    bmiresults.style.color = "yellow";
+} else {
+    bmiresults.innerHTML = "You are overweight.";
+    bmiresults.style.color = "green";
+}
+
 });
                                                                //in the input that is why we have to convert  
